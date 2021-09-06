989f7dad2c21a97ae0dad3af1b646d4b9b9e2e7d
Niroshan-Krishnamoorthy <niro.dev97@gmail.com>
2021-09-06 17:35:21 +0530

    match page initiated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import { TeamPage } from './pages/TeamPage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { MatchPage } from './pages/MatchPage';
 
 function App() {
   return (
     <div className="App">
-      <TeamPage />
+      <Router>
+        <Switch>
+          <Route path="/teams/:teamName/matches/:year" component={MatchPage} />
+          <Route path="/teams/:teamName" component={TeamPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import {useParams} from 'react-router-dom'
 import { MatchDetailCard } from '../components/MatchDetailCard'
 import { MatchSmallCard } from '../components/MatchSmallCard'
 import teamService from '../services/TeamService'
@@ -7,22 +8,28 @@ export const TeamPage = () => {
 
     const [team, setTeam] = useState({ matches: [] })
 
+    const {teamName} = useParams()
+
     useEffect(() => {
         const fetchData = async () => {
-            const response = await teamService.getTeamData("Delhi Capitals");
+            const response = await teamService.getTeamData(teamName);
             const data = await response.json()
             setTeam(data)
         }
         fetchData()
 
-    }, [])
+    }, [teamName])
+
+    if(!team || !team.teamName){
+        return <h1> Team Not Found! </h1>
+    }
 
     return (
         <div className="TeamPage">
             <h1>{team.teamName}</h1>
-            <MatchDetailCard match={team.matches[0]}/>
+            <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
             {
-                team.matches.slice(1).map(match => <MatchSmallCard key={match.id} match={match}/>)
+                team.matches.slice(1).map(match => <MatchSmallCard key={match.id} teamName={team.teamName} match={match}/>)
             }
         </div>
     )
